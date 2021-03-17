970fac824adb8036a501fb42e7a76722c4167326
Syed Anus Ali <49055757+anusali04928@users.noreply.github.com>
2021-03-18 02:42:47 +0500

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'yoursecrets',
-    database: 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.flowl.mongodb.net/testdb?retryWrites=true&w=majority'
+    database: 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.flowl.mongodb.net/Thalihelp?retryWrites=true&w=majority'
 }
